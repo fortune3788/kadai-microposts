b4fb789f85eec6df5c89ab1c0adaac0e5bf1157d
fortune <basket85r@gmail.com>
2022-03-26 03:04:44 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def create
+    @user = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'お気に入りに追加しました'
+    redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'お気に入りから解除しました'
+    redirect_to @user
+  end
+end
